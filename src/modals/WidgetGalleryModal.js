c82f8a43bd73b532213434a760ba60c9428290bd
Biku213 <kunwarbindu220@gmail.com>
2023-07-03 17:03:42 +0545

    modified the code

--- src/modals/WidgetGalleryModal.js
+++ src/modals/WidgetGalleryModal.js
@@ -3,6 +3,7 @@ import ClockWidget from '../widgets/ClockWidget'
 import ReminderListWidget from '../widgets/ReminderListWidget'
 import TimerWidget from '../widgets/TimerWidget'
 import CalendarWidget from '../widgets/CalendarWidget' 
+import TodoListWidget from '../widgets/MyCustomWidget'
 
 
 export default function WidgetGalleryModal({ setShowWidgetModal, selectedWidgetArea, widgets, setWidgets,  }) {
@@ -11,6 +12,9 @@ export default function WidgetGalleryModal({ setShowWidgetModal, selectedWidgetA
     { component: <ReminderListWidget />, name: "Reminder List" },
     { component: <TimerWidget />, name: "Timer" },
     { component: <CalendarWidget />, name: "Calendar" },
+    { component: <TodoListWidget />, name: "TodoList" },
+    
+
     
   ])
   return (
